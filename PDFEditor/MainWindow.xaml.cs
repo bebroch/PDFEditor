d8ffce2cca1068dc22ffb25b43427d69a1e3e7e9
bebroch <97059293+bebroch@users.noreply.github.com>
2023-03-03 00:21:16 +0300

    2
    появились кнопки, которые перелистывают страницы

--- PDFEditor/MainWindow.xaml.cs
+++ PDFEditor/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using IronPdf;
 
 namespace PDFEditor
@@ -30,13 +19,15 @@ namespace PDFEditor
         {
             InitializeComponent();
         }
-
+        
         // Кидаем в программу файл и выводим его.
         private void MainPanel_Drop(object sender, DragEventArgs e)
         {
             string[] path = (string[])e.Data.GetData(DataFormats.FileDrop);
             document = new PdfDocument(path[0]);
             pagePDFInPng = new ImageSource[document.PageCount];
+            allPages.Content = document.PageCount;
+            nowPage.Content = 1;
             ConvertPDFtoImage(0);
         }
 
@@ -48,35 +39,52 @@ namespace PDFEditor
             PictureBox.Source = imgsourc;
             pagePDFInPng[page] = PictureBox.Source;
         }
-
+        
         private int DPCPlus = 0;
         private int DPCMinus = 0;
         private int docPageCount = 0;
 
+        // Кнопка переключения страницы вперёд
+        private void Button_Click_Next(object sender, RoutedEventArgs e)
+        {
+            KeyDown(Key.Right);
+        }
+
+        // Кнопка переключения страницы назад
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            KeyDown(Key.Left);
+        }
+
         // Переключение PDF по сраницам
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Right || e.Key == Key.Left)
+            KeyDown(e.Key);
+        }
+
+        private new void KeyDown(Key key)
+        {
+            if (document != null && (key == Key.Right || key == Key.Left))
             {
                 DPCPlus = docPageCount + 1 <= document.PageCount - 1 ? docPageCount + 1 : 0;
                 DPCMinus = docPageCount - 1 >= 0 ? docPageCount - 1 : document.PageCount - 1;
 
-                if (e.Key == Key.Right && pagePDFInPng[DPCPlus] != null)
+                if (key == Key.Right && pagePDFInPng[DPCPlus] != null)
                 { PictureBox.Source = pagePDFInPng[DPCPlus]; docPageCount++; }
-                else if (e.Key == Key.Left && pagePDFInPng[DPCMinus] != null)
+                else if (key == Key.Left && pagePDFInPng[DPCMinus] != null)
                 { PictureBox.Source = pagePDFInPng[DPCMinus]; docPageCount--; }
 
-
-                if (e.Key == Key.Right && pagePDFInPng[DPCPlus] == null)
+                if (key == Key.Right && pagePDFInPng[DPCPlus] == null)
                 { ConvertPDFtoImage(DPCPlus); docPageCount++; }
-                else if (e.Key == Key.Left && pagePDFInPng[DPCMinus] == null)
+                else if (key == Key.Left && pagePDFInPng[DPCMinus] == null)
                 { ConvertPDFtoImage(DPCMinus); docPageCount--; }
-
 
                 if (docPageCount < 0)
                     docPageCount = document.PageCount - 1;
                 else if (docPageCount > document.PageCount - 1)
                     docPageCount = 0;
+
+                nowPage.Content = (docPageCount + 1).ToString();
             }
         }
     }
